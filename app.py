a2f476a44dc6c3525cdc28eae61e648362a8a027
Gerald Sadya <sadyageraldm@gmail.com>
2025-06-02 23:42:43 +0200

    Add Redis caching logic with 12-hour expiry and fake fallback

--- app.py
+++ app.py
@@ -1,17 +1,37 @@
-#main code
-
-#app.py
 from flask import Flask, jsonify
+import redis
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
+#Connecting to Redis
+redis_url = os.getenv("REDIS_URL", "redis://localhost:6379")
+cache = redis.Redis.from_url(redis_url)
+
 @app.route('/weather/<city_code>', methods=['GET'])
 def get_weather(city_code):
-    # TEMPORARY HARDCODED RESPONSE
+    # Redis Cache
+    cached_data = cache.get(city_code)
+
+    if cached_data:
+        return jsonify({
+            'city': city_code,
+            'data': cached_data.decode('utf-8'),
+            'source': 'cache'
+        })
+
+    #fake response
+    fake_weather_data = '25°C, Sunny'
+
+    cache.setex(city_code, 43200, fake_weather_data)#12 hour expiry
+
     return jsonify({
         'city': city_code,
-        'temperature': '25°C',
-        'condition': 'Sunny'
+        'data': fake_weather_data,
+        'source': 'api (fake)'
     })
 
 if __name__ == '__main__':
